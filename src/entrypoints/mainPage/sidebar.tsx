4ebdcbe8a1cda4907dbda8994236e6c9563f4bc5
bluolightning <124224350+bluolightning@users.noreply.github.com>
2025-01-24 17:53:48 -0500

    Remove the breadcrumb chain from the header

--- src/entrypoints/mainPage/sidebar.tsx
+++ src/entrypoints/mainPage/sidebar.tsx
@@ -1,15 +1,5 @@
 'use client';
 import React from 'react';
-import { useState } from 'react';
-import { AppSidebar } from '../../components/app-sidebar';
-import {
-    Breadcrumb,
-    BreadcrumbItem,
-    BreadcrumbLink,
-    BreadcrumbList,
-    BreadcrumbPage,
-    BreadcrumbSeparator,
-} from '@/components/ui/breadcrumb';
 import { Separator } from '@/components/ui/separator';
 import {
     SidebarInset,
@@ -18,11 +8,8 @@ import {
 } from '@/components/ui/sidebar';
 
 export default function Page() {
-    const [activeSection, setActiveSection] = useState<string>('');
-
     return (
         <SidebarProvider>
-            <AppSidebar onSectionChange={setActiveSection} />
             <SidebarInset>
                 <header className="flex h-16 shrink-0 items-center gap-2 border-b">
                     {/* Header stuff */}
@@ -32,21 +19,6 @@ export default function Page() {
                             orientation="vertical"
                             className="mr-2 h-4"
                         />
-                        <Breadcrumb>
-                            <BreadcrumbList>
-                                <BreadcrumbItem className="hidden md:block">
-                                    <BreadcrumbLink href="#">
-                                        Building Your Application
-                                    </BreadcrumbLink>
-                                </BreadcrumbItem>
-                                <BreadcrumbSeparator className="hidden md:block" />
-                                <BreadcrumbItem>
-                                    <BreadcrumbPage>
-                                        {activeSection}
-                                    </BreadcrumbPage>
-                                </BreadcrumbItem>
-                            </BreadcrumbList>
-                        </Breadcrumb>
                     </div>
                 </header>
                 <div className="flex flex-1 flex-col gap-4 p-4">
